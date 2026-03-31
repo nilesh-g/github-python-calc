327a2798c203065b5f043e315a3d3f2c0b3a15d6
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-03-31 14:30:56 +0530

    calc menu

---
+++ calc.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter number1: "))
+num2 = int(input("enter number2: "))
+
+choice = int(input("0. exit\nenter choice: "))
+
+if choice == 0:
+    print("bye!")
+else:
+    print("invalid choice!")
