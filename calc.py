0de65eda9460681009bfda553af033aebc178f4f
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-03-31 14:46:12 +0530

    subtract fn

--- calc.py
+++ calc.py
@@ -1,12 +1,15 @@
 num1 = int(input("enter number1: "))
 num2 = int(input("enter number2: "))
 
-choice = int(input("0. exit\n1. add\nenter choice: "))
+choice = int(input("0. exit\n1. add\n2. subtract\nenter choice: "))
 
 if choice == 0:
     print("bye!")
 elif choice == 1:
     result = num1 + num2
     print("add result =", result)
+elif choice == 2:
+    result = num1 - num2
+    print("subtract result =", result)
 else:
     print("invalid choice!")
