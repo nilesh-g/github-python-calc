deb9b4a12ec38c46a3d024b55d2adcdb3f23a8ca
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-03-31 14:35:34 +0530

    add fn

--- calc.py
+++ calc.py
@@ -1,9 +1,12 @@
 num1 = int(input("enter number1: "))
 num2 = int(input("enter number2: "))
 
-choice = int(input("0. exit\nenter choice: "))
+choice = int(input("0. exit\n1. add\nenter choice: "))
 
 if choice == 0:
     print("bye!")
+elif choice == 1:
+    result = num1 + num2
+    print("add result =", result)
 else:
     print("invalid choice!")
